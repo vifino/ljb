95bbb67ad90e480b21534a554864989efaebba9a
Adrian Pistol <adinator1999@gmail.com>
2015-02-02 20:38:30 +0100

    Derped.

--- build.lua
+++ build.lua
@@ -3,5 +3,5 @@ function run(cmd)
 	print("$ "..cmd)
 	os.execute(cmd)
 end
-run("luajit bin/ljb -smc bin/ljb modules/helloworld.lua")
+run("luajit bin/ljb -smc bin/ljb ljb modules/helloworld.lua")
 print("Done.")
